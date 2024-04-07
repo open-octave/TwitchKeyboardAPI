dca71e380d4c5f22aae6d759184ce7cee63509b0
PCDSandwichMan <46288251+PCDSandwichMan@users.noreply.github.com>
2024-04-07 00:00:00 +0000

    feat(root): B8B5DF6E-2231-417F-856A-696893446300

--- twitch_emulator/twitch_bot.py
+++ twitch_emulator/twitch_bot.py
@@ -41,10 +41,88 @@ class TwitchBot:
 
     def execute_command(self, command_function):
         try:
+            logging.info("Focusing on window")
             self.windowManagerClient.focus_on_window()
+            logging.info("Window focused")
+
+            logging.info(f"Executing command: {command_function.__name__}")
             command_function()
+            logging.info(f"Command executed: {command_function.__name__}")
+
         except Exception as e:
             logging.error(f"Error executing command: {e}")
+
+    def handle_up_command(self):
+        try:
+            logging.info("Move up command received")
+            pyautogui.press("up")
+            logging.info("Move up command handled")
+        except Exception as e:
+            logging.error(f"Error handling up command: {e}")
+
+    def handle_down_command(self):
+        try:
+            logging.info("Move down command received")
+            pyautogui.press("down")
+            logging.info("Move down command handled")
+        except Exception as e:
+            logging.error(f"Error handling down command: {e}")
+
+    def handle_left_command(self):
+        try:
+            logging.info("Move left command received")
+            pyautogui.press("left")
+            logging.info("Move left command handled")
+        except Exception as e:
+            logging.error(f"Error handling left command: {e}")
+
+    def handle_right_command(self):
+        try:
+            logging.info("Move right command received")
+            pyautogui.press("right")
+            logging.info("Move right command handled")
+        except Exception as e:
+            logging.error(f"Error handling right command: {e}")
+
+    def handle_a_command(self):
+        try:
+            logging.info("A command received")
+            pyautogui.press("x")
+            logging.info("A command handled")
+        except Exception as e:
+            logging.error(f"Error handling A command: {e}")
+
+    def handle_b_command(self):
+        try:
+            logging.info("B command received")
+            pyautogui.press("z")
+            logging.info("B command handled")
+        except Exception as e:
+            logging.error(f"Error handling B command: {e}")
+
+    def handle_y_command(self):
+        try:
+            logging.info("Y command received")
+            pyautogui.press("v")
+            logging.info("Y command handled")
+        except Exception as e:
+            logging.error(f"Error handling Y command: {e}")
+
+    def handle_x_command(self):
+        try:
+            logging.info("X command received")
+            pyautogui.press("c")
+            logging.info("X command handled")
+        except Exception as e:
+            logging.error(f"Error handling X command: {e}")
+
+    def handle_start_command(self):
+        try:
+            logging.info("Start command received")
+            pyautogui.press("enter")
+            logging.info("Start command handled")
+        except Exception as e:
+            logging.error(f"Error handling Start command: {e}")
 
     def on_public_message(self, connection, event):
         try:
@@ -54,88 +132,16 @@ class TwitchBot:
             command = args[0] if args else None
             command = command.lower() if command else None
 
-            def handle_up_command():
-                try:
-                    logging.info("Move up command received")
-                    pyautogui.press("up")
-                    logging.info("Move up command handled")
-                except Exception as e:
-                    logging.error(f"Error handling up command: {e}")
-
-            def handle_down_command():
-                try:
-                    logging.info("Move down command received")
-                    pyautogui.press("down")
-                    logging.info("Move down command handled")
-                except Exception as e:
-                    logging.error(f"Error handling down command: {e}")
-
-            def handle_left_command():
-                try:
-                    logging.info("Move left command received")
-                    pyautogui.press("left")
-                    logging.info("Move left command handled")
-                except Exception as e:
-                    logging.error(f"Error handling left command: {e}")
-
-            def handle_right_command():
-                try:
-                    logging.info("Move right command received")
-                    pyautogui.press("right")
-                    logging.info("Move right command handled")
-                except Exception as e:
-                    logging.error(f"Error handling right command: {e}")
-
-            def handle_a_command():
-                try:
-                    logging.info("A command received")
-                    pyautogui.press("x")
-                    logging.info("A command handled")
-                except Exception as e:
-                    logging.error(f"Error handling A command: {e}")
-
-            def handle_b_command():
-                try:
-                    logging.info("B command received")
-                    pyautogui.press("z")
-                    logging.info("B command handled")
-                except Exception as e:
-                    logging.error(f"Error handling B command: {e}")
-
-            def handle_y_command():
-                try:
-                    logging.info("Y command received")
-                    pyautogui.press("v")
-                    logging.info("Y command handled")
-                except Exception as e:
-                    logging.error(f"Error handling Y command: {e}")
-
-            def handle_x_command():
-                try:
-                    logging.info("X command received")
-                    pyautogui.press("c")
-                    logging.info("X command handled")
-                except Exception as e:
-                    logging.error(f"Error handling X command: {e}")
-
-            def handle_start_command():
-                try:
-                    logging.info("Start command received")
-                    pyautogui.press("enter")
-                    logging.info("Start command handled")
-                except Exception as e:
-                    logging.error(f"Error handling Start command: {e}")
-
             commands = {
-                "up": handle_up_command,
-                "down": handle_down_command,
-                "left": handle_left_command,
-                "right": handle_right_command,
-                "a": handle_a_command,
-                "b": handle_b_command,
-                "y": handle_y_command,
-                "x": handle_x_command,
-                "start": handle_start_command,
+                "up": self.handle_up_command,
+                "down": self.handle_down_command,
+                "left": self.handle_left_command,
+                "right": self.handle_right_command,
+                "a": self.handle_a_command,
+                "b": self.handle_b_command,
+                "y": self.handle_y_command,
+                "x": self.handle_x_command,
+                "start": self.handle_start_command,
             }
 
             if command in commands:
