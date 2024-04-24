6ae690e39cc0bf719fabc9cad98832a5512a02bc
PCDSandwichMan <46288251+PCDSandwichMan@users.noreply.github.com>
2024-04-24 00:00:00 +0000

    feat(root): 48251636-A438-46C0-B5AD-DCD3EE465EEB

--- twitch_emulator/__main__.py
+++ twitch_emulator/__main__.py
@@ -1,23 +1,15 @@
 import logging
-import os
-
-from dotenv import load_dotenv
 
 from twitch_emulator.twitch_bot import TwitchBot
+
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-load_dotenv()
 
-USERNAME = os.getenv("TWITCH_USERNAME")
-PASSWORD = os.getenv("TWITCH_OAUTH_TOKEN")
-SERVER = "irc.chat.twitch.tv"
-PORT = 6667
-CHANNEL = os.getenv("TWITCH_CHANNEL")
-CHANNELS = [f"#{CHANNEL}"]
+CHANNEL = "pcdsandwichman"
 
-twitch_bot = TwitchBot(USERNAME, PASSWORD, SERVER, PORT, CHANNELS)
+twitch_bot = TwitchBot(CHANNEL)
 
 
 def start_twitch_listener():
@@ -25,13 +17,6 @@ def start_twitch_listener():
         twitch_bot.start()
     except Exception as e:
         logging.error(f"Error starting the bot: {e}")
-
-
-def stop_twitch_listener():
-    try:
-        twitch_bot.stop()
-    except Exception as e:
-        logging.error(f"Error stopping the bot: {e}")
 
 
 if __name__ == "__main__":
