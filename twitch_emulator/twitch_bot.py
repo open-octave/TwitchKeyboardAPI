79243eed9c350c8bf39f96eb6c9805cc0f9bb110
PCDSandwichMan <46288251+PCDSandwichMan@users.noreply.github.com>
2024-05-03 00:00:00 +0000

    feat(root): 5A0CF044-1FFC-45F5-8D3D-085384FCDA9E

--- twitch_emulator/twitch_bot.py
+++ twitch_emulator/twitch_bot.py
@@ -4,6 +4,8 @@ import irc.bot
 import pydirectinput
 
 from twitch_emulator.window_manager import WindowManager
+from threading import Timer
+import uuid
 
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
@@ -16,14 +18,27 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         self.channel = "#" + channel
         self.windowManagerClient = WindowManager()
 
+        self.currentlyHeldKey = None
+
     def on_welcome(self, connection, event):
+        """
+        This function will handle the bot connecting to the server. It will log the event.
+        """
         logging.info(f"Joining channel: {self.channel}")
         connection.join(self.channel)
 
     def on_join(self, connection, event):
+        """
+        This function will handle the bot joining the channel. It will log the event.
+        """
         logging.info(f"Joined channel: {self.channel}")
 
     def on_pubmsg(self, connection, event):
+        """
+        This function will handle public messages sent in the chat. It will parse the message
+        and execute the appropriate command.
+        """
+
         author = irc.strings.lower(event.source.nick)
         message = event.arguments[0]
         logging.info(f"{author}: {message}")
@@ -44,29 +59,47 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 "b": self.handle_b_command,
                 "y": self.handle_y_command,
                 "x": self.handle_x_command,
+                "start": self.handle_start_command,
+                "select": self.handle_select_command,
+                "!hold": self.handle_hold_command,
+                "!release": self.handle_release_command,
+                "!fastForwardToggle": self.handle_fast_forward_toggle_command,
             }
 
             if command in commands:
-                self.execute_command(commands[command])
+                self.execute_command(commands[command], message)
                 logging.info(f"Command executed: {command}")
 
         except Exception as e:
             logging.error(f"Error processing public message: {e}")
 
-    def execute_command(self, command_function):
+    def execute_command(self, command_function, message):
+        """
+        This function will focus on the retroarch window and execute the command function
+        passed in as a parameter.
+
+        :param command_function: The function to execute
+        :param message: The message to pass to the command function
+        """
+
         try:
             logging.info("Focusing on window")
             self.windowManagerClient.focus_on_retroarch()
             logging.info("Window focused")
 
             logging.info(f"Executing command: {command_function.__name__}")
-            command_function()
+            command_function(message)
             logging.info(f"Command executed: {command_function.__name__}")
 
         except Exception as e:
             logging.error(f"Error executing command: {e}")
 
     def handle_up_command(self):
+        """
+        This function will handle the up command by pressing the up arrow key.
+        Example message: "up"
+        """
+
         try:
             logging.info("Move up command received")
             pydirectinput.press("up")
@@ -75,6 +108,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling up command: {e}")
 
     def handle_down_command(self):
+        """
+        This function will handle the down command by pressing the down arrow key.
+        Example message: "down"
+        """
+
         try:
             logging.info("Move down command received")
             pydirectinput.press("down")
@@ -83,6 +121,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling down command: {e}")
 
     def handle_left_command(self):
+        """
+        This function will handle the left command by pressing the left arrow key.
+        Example message: "left"
+        """
+
         try:
             logging.info("Move left command received")
             pydirectinput.press("left")
@@ -91,6 +134,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling left command: {e}")
 
     def handle_right_command(self):
+        """
+        This function will handle the right command by pressing the right arrow key.
+        Example message: "right"
+        """
+
         try:
             logging.info("Move right command received")
             pydirectinput.press("right")
@@ -99,6 +147,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling right command: {e}")
 
     def handle_a_command(self):
+        """
+        This function will handle the A command by pressing the X key.
+        Example message: "a"
+        """
+
         try:
             logging.info("A command received")
             pydirectinput.press("x")
@@ -107,6 +160,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling A command: {e}")
 
     def handle_b_command(self):
+        """
+        This function will handle the B command by pressing the Z key.
+        Example message: "b"
+        """
+
         try:
             logging.info("B command received")
             pydirectinput.press("z")
@@ -115,6 +173,11 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling B command: {e}")
 
     def handle_y_command(self):
+        """
+        This function will handle the Y command by pressing the A key.
+        Example message: "y"
+        """
+
         try:
             logging.info("Y command received")
             pydirectinput.press("a")
@@ -123,9 +186,104 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             logging.error(f"Error handling Y command: {e}")
 
     def handle_x_command(self):
+        """
+        This function will handle the X command by pressing the S key.
+        Example message: "x"
+        """
+
         try:
             logging.info("X command received")
             pydirectinput.press("s")
             logging.info("X command handled")
         except Exception as e:
             logging.error(f"Error handling X command: {e}")
+
+    def handle_start_command(self):
+        """
+        This function will handle the start command by pressing the enter key.
+        Example message: "!start"
+        """
+
+        try:
+            logging.info("Start command received")
+            pydirectinput.press("enter")
+            logging.info("Start command handled")
+        except Exception as e:
+            logging.error(f"Error handling Start command: {e}")
+
+    def handle_select_command(self):
+        """
+        This function will handle the select command by pressing the shift key.
+        Example message: "!select"
+        """
+
+        try:
+            logging.info("Select command received")
+            pydirectinput.press("shift")
+            logging.info("Select command handled")
+        except Exception as e:
+            logging.error(f"Error handling Select command: {e}")
+
+    def handle_hold_command(self, message):
+        """
+        This function will hold a key down until a release command is received or
+        the fallback timer of 30 seconds expires. For now this will only allow directional
+        keys to be held down.
+
+        Example message: "!hold up"
+        """
+
+        try:
+            if self.currentlyHeldKey:
+                self.handle_release_command()
+
+            allowed_keys = ["up", "down", "left", "right"]
+            args = message.split()
+            key = args[1] if len(args) > 1 else None
+            key = key.lower() if key else None
+
+            if key in allowed_keys:
+                logging.info(f"Hold command received: {key}")
+                pydirectinput.keyDown(key)
+                self.currentlyHeldKey = key
+
+                # TODO matt: handle timeout
+
+                logging.info(f"Hold command handled: {key}")
+            else:
+                logging.error(f"Invalid key received: {key}")
+
+        except Exception as e:
+            logging.error(f"Error handling hold command: {e}")
+
+    def handle_release_command(self):
+        """
+        This function will release the key that is currently being held down.
+        Example message: "!release"
+        """
+
+        try:
+            if not self.currentlyHeldKey:
+                logging.error("No key is currently being held down")
+                return
+
+            logging.info("Release command received")
+            pydirectinput.keyUp(self.currentlyHeldKey)
+            self.currentlyHeldKey = None
+            logging.info("Release command handled")
+
+        except Exception as e:
+            logging.error(f"Error handling release command: {e}")
+
+    def handle_fast_forward_toggle_command(self):
+        """
+        This function will handle the fast forward toggle command by pressing the space key.
+        Example message: "!fastForwardToggle"
+        """
+
+        try:
+            logging.info("Fast forward toggle command received")
+            pydirectinput.press("space")
+            logging.info("Fast forward toggle command handled")
+        except Exception as e:
+            logging.error(f"Error handling fast forward toggle command: {e}")
